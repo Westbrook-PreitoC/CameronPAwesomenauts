413143f788d0fd0653d23cafa6b755d011d04aaa
Cameron Preito <cameotheman@yahoo.com>
2015-03-23 17:31:09 -0700

    next commit

--- js/game.js
+++ js/game.js
@@ -41,7 +41,7 @@ var game = {
 	"loaded" : function () {
                 me.pool.register("player", game.PlayerEntity, true); 
                 me.pool.register("PlayerBase", game.PlayerBaseEntity);
-                //me.pool.register("EnemyBase", game.EnemyBaseEntity);
+                me.pool.register("EnemyBase", game.EnemyBaseEntity);
                 me.pool.register("EnemyCreep", game.EnemyCreep, true);
                 me.pool.register("GameManager", game.GameManager);
             
