e2581a6d67dacf5334e673e9cd8ecacf8e9fb32a
Cameron Preito <cameotheman@yahoo.com>
2015-04-07 15:03:30 -0700

    next commit

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -40,5 +40,30 @@ game.HeroDeathManager = Object.extend({
             me.game.world.removeChild(game.data.player);
             me.state.current().resetPlayer(10, 0);
         }
+        
+        return true;
    }
 });
+
+game.ExperienceManager = Object.extend({
+   init: function(x, y, settings){
+       this.alwaysUpdate = true;
+       this.gameOver = false;
+   },
+   
+   update: function(){
+       if(game.data.win === true && !this.gameOver){
+           game.data.exp += 10;
+           this.gameOver = true;
+       }else if(game.data.win === false && !this.gameOver){
+           game.data.exp += 1;
+           this.gameOver = true;
+       }
+       console.log(game.data.exp);
+       
+       return true;
+   }
+    
+});
+
+
